4ae22e0632874a81d677361bee85ba0448d3de90
Carlo <carlo@Carlos-MacBook-Air.local>
2023-05-13 19:56:18 +0800

    Removed test files

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,12 @@
 <?php
 
-// routes.php
 return [
-    'GET' => [
-        '' => 'TaskController@index',
-        'tasks/create' => 'TaskController@create',
-        'tasks/{id}' => 'TaskController@show',
-        'tasks/{id}/edit' => 'TaskController@edit',
+    'get' => [
+        '' => 'HomeController@index',
+        'about' => 'AboutController@index',
+        // ... other GET routes ...
     ],
-    'POST' => [
-        'tasks' => 'TaskController@store',
-        'tasks/{id}' => 'TaskController@update',
-        'tasks/{id}/delete' => 'TaskController@delete',
+    'post' => [
+        // ... other POST routes ...
     ],
 ];
